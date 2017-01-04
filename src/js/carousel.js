ddedd2271d52ac20b2a31f9f25be68905fb7dfff
singhsukh <sukhwinder.webexpert@gmail.com>
2017-01-04 10:27:03 +0530

    first preview

---
+++ src/js/carousel.js
@@ -0,0 +1,17 @@
+var checkitem = function() {
+  var $this;
+  $this = $("#myCarousel");
+  if ($("#myCarousel .carousel-inner .item:first").hasClass("active")) {
+    $this.children(".left").hide();
+    $this.children(".right").show();
+  } else if ($("#myCarousel .carousel-inner .item:last").hasClass("active")) {
+    $this.children(".right").hide();
+    $this.children(".left").show();
+  } else {
+    $this.children(".carousel-control").show();
+  }
+};
+
+checkitem();
+
+$("#myCarousel").on("slid.bs.carousel", "", checkitem);
